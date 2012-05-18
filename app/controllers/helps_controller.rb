8439a1edd469a699e3f23377e4c470c57e1e2b67
keating <outofclouds@gmail.com>
2012-05-18 21:36:36 +0800

    fix bug of answer

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,5 +1,8 @@
 class HelpsController < ApplicationController
 
+  before_filter :signed_in_user, :only => [:create]
+  before_filter :correct_user, :only => [:destroy]
+
   def create
     @question = Question.find params[:help][:question_id]
     @help = Help.new(params[:help])
@@ -18,5 +21,12 @@ class HelpsController < ApplicationController
     @help.destroy
 
     redirect_to @question
+  end
+
+  private
+
+  def correct_user
+    @help = Help.find(params[:id])
+    redirect_to(root_path) unless ((current_user? @help.user) or (current_user and current_user.admin?))
   end
 end
