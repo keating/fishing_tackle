721459ea828c1da3ca53c4feefb373470441e3d9
keating <outofclouds@gmail.com>
2012-05-20 15:35:52 +0800

    share and disqus

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @suppliers = Supplier.all_by_created_time.limit(5)
-    @questions = Question.all_by_created_time.limit(5)
+    @suppliers = Supplier.all_by_created_time.limit(10)
+    @questions = Question.all_by_created_time.limit(10)
   end
 end
